476e89157810de27b8dbfd9b6231eadd5f5b5077
Isaac Freeman <isaac@freeman.org.nz>
2022-02-17 18:08:42 +1300

    Add mdoel specs

--- spec/models/garden_bed_spec.rb
+++ spec/models/garden_bed_spec.rb
@@ -14,13 +14,35 @@ RSpec.describe GardenBed, type: :model do
       expect(subject).to eq "Lettuce, Tomato"
     end
 
-    context "whent here's more than one plant for a species" do
+    context "when there's more than one plant for a species" do
       let(:garden_bed) { FactoryBot.create :garden_bed, plants: [lettuce_plant1, lettuce_plant2, tomato_plant] }
       let(:lettuce_plant2) { FactoryBot.create :plant, species: lettuce }
 
       it "lists each species once" do
         expect(subject).not_to eq "Lettuce, Lettuce, Tomato"
       end
+    end
+  end
+
+  describe "#propagate_weeds!" do
+    subject { garden_bed.propagate_weeds! }
+
+    let!(:plant) { FactoryBot.create :plant, garden_bed: garden_bed, species: lettuce }
+    let!(:weed_species) { FactoryBot.create :species, weed: true }
+    let(:lettuce) { FactoryBot.create :species, name: "Lettuce", weed: false }
+    let(:garden_bed) { FactoryBot.create :garden_bed, last_weeded_at: 3.days.ago }
+
+    before do
+      # Weed propagation is normally random, which could give us different
+      # results each time this test is run. To ensure we always get some weeds
+      # we're overriding SecureRandom to give the same value every time we run
+      # the test
+      allow(SecureRandom).to receive(:random_number).and_return(0.05)
+    end
+
+    it "adds weeds to the garden bed" do
+      expect { subject }.to change { garden_bed.plants.count }
+      expect(garden_bed.plants.order(:created_at).last.species.weed?).to be true
     end
   end
 end
--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#weed!" do
+    subject { plant.weed! }
+
+    let!(:plant) { FactoryBot.create(:plant, garden_bed: garden_bed, species: species) }
+    let(:garden_bed) { FactoryBot.create(:garden_bed) }
+
+    context "when the plan is a weed" do
+      let(:species) { FactoryBot.create(:species, weed: true) }
+
+      it "destroys the plant" do
+        expect { subject }.to change { Plant.count }.from(1).to(0)
+      end
+
+      it "updates the garden bed's last_weeded_at timestamp" do
+        expect { subject }.to change { garden_bed.last_weeded_at }
+      end
+    end
+
+    context "when the plant is not a weed" do
+      let(:species) { FactoryBot.create(:species, weed: false) }
+
+      it "raises an error"
+    end
+  end
 end
